fc5ca9d072f6dca11e350484550e0f32c2a8fd36
Pete Gosling <petegoz@gmail.com>
2021-10-31 09:53:27 +0000

    Add operations project with interfaces for command, query, reader and writer. Add model project for implementations.

---
+++ Model/CreateLocationCommand.cs
@@ -0,0 +1,13 @@
+﻿using Operations;
+using System;
+
+namespace Model
+{
+    public class CreateLocationCommand: ICommand<Location>
+    {
+        public Location Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Operations/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Operations
+{
+    public interface ICommand<out T>
+    {
+        public T Run();
+    }
+}
