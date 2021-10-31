25d1e973848e5bfb5c30a71760ab8b2c606d959d
Pete Gosling <petegoz@gmail.com>
2021-10-31 07:02:24 +0000

    Create Airbox solution with API project.

---
+++ Api/Controllers/LocationsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ILogger<LocationsController> _logger;
+
+        public LocationsController(ILogger<LocationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public OkObjectResult Get()
+        {
+            return Ok("API Working");
+        }
+    }
+}
